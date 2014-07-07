301d1db0cf51c9619602fd7e0f817a48d72facf0
blooies <joy28@msn.com>
2014-07-06 20:06:23 -0400

    add some animations for homescreen

--- initialize.js
+++ initialize.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
 
-  appController = new ApplicationController(new PrepTableController(), new OvenController(new Oven(), new OvenView()));
+	appController = new ApplicationController(new PrepTableController(), new OvenController(new Oven(), new OvenView()));
 
- 
+
+	$('h2').addClass('animated bounceTop')
+	$('#play_button').addClass('animated bounceTop')
+
+	$('#play_button').on('click', function() {
+		$('.intro_game').addClass('animated_fast slideDown')
+
+		$('.intro_game').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+			$('.intro_game').addClass('hidden');
+			$('.menu_group').addClass('showing animated_fast bounceTop')
+		});
+		
+
+	})
 });
